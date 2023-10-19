f4cf9f2f3f3a7ae11074357c3b2544e3e2bfb5da
akshay-vs <akshay.vs2005@gmail.com>
2023-10-19 09:28:40 +0530

    Feat: Migrating python websocket listener

--- Server/static/Round 3/tests/test.default.py
+++ Server/static/Round 3/tests/test.default.py
@@ -5,16 +5,10 @@ sys.path.append("..")
 from problem import add
 
 test_set = [
-    {"args": (1,2), "expected": 3},
-    {"args": (3, ), "expected": 7},
+    {"args": (1, 2), "expected": 3},
+    {"args": (3, 4), "expected": 7},
     {"args": (5, 6), "expected": 11},
 ]
-
-try: add(1, 2)
-except Exception as e: 
-    print(e)
-    sys.exit(1)
-
 
 test_suite = TestSuite(add, test_set)
 test_suite.run()
